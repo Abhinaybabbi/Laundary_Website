4561f436b4565ed457a17105becdb9c2d2a6821c
Abhinay babbi <30630095+Abhinaybabbi@users.noreply.github.com>
2021-10-31 16:35:40 +0530

    abhinay

--- Server/index.js
+++ Server/index.js
@@ -5,7 +5,7 @@ const app = express();
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 require('dotenv').config()
-    // Sam
+    // Sam Abhinay
 
 mongoose.connect(`${process.env.MongodbServer}`, (err, then) => {
     if (err) {
