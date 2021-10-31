10ac14aba529f56dbfc00a80a8f420f66e27c544
Abhinay babbi <30630095+Abhinaybabbi@users.noreply.github.com>
2021-10-31 15:49:20 +0530

    connected to db

--- Server/index.js
+++ Server/index.js
@@ -4,12 +4,17 @@ const mongoose = require("mongoose");
 const app = express();
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
+const dotenv = require("dotenv");
 
-mongoose.connect(MongoDBServer);
-
-app.get("/",(req,res)=>{
+mongoose.connect("mongodb+srv://adminab:zxcvbnm123@sandbox.tjibg.mongodb.net/Laundary_website?retryWrites=true&w=majority",(err,res)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log("connected to db")
+    }
 
 });
+
 
 
 app.listen("5000",()=>console.log("server is running in 5K"));
