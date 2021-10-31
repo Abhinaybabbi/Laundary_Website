57dc24d44bb7176905a38f1255411f154ae6c98b
subhan kakarla <kakarlasubhan7@gmail.com>
2021-10-31 16:30:29 +0530

    su

---
+++ Client/src/components/pages/navBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = ()=>{
+    return(
+             
+     <div>  
+
+    <nav>
+    <h3>LAUNDRY</h3>
+        
+    </nav>
+    </div>
+ 
+    )
+}
+export default Navbar
