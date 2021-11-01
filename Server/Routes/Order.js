9926c83b9f7ba094c7e08276c73192980a9c183d
Abhinay babbi <30630095+Abhinaybabbi@users.noreply.github.com>
2021-11-01 15:48:15 +0530

    changed the timestamp in order model

---
+++ Server/Routes/Order.js
@@ -0,0 +1,16 @@
+const { json } = require("body-parser");
+const express = require("express");
+const Order = require("../Models/order");
+
+const Orders = require("../Models/order");
+
+const router = express.Router();
+
+
+router.put("/:id",async function(req,res){
+
+});
+
+
+
+module.exports=router;
