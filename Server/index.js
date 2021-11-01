8d92565fc112157b7fe5dbc441c767a355272115
subhan kakarla <kakarlasubhan7@gmail.com>
2021-11-01 18:57:40 +0530

    minor

--- Server/index.js
+++ Server/index.js
@@ -4,21 +4,19 @@ const mongoose = require("mongoose");
 const app = express();
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-require('dotenv').config()
-
-
-app.use(express.json())
-
-require('./Models/user')
-app.use(require('./Routes/auth'))
-
+require("dotenv").config();
 
 mongoose.connect(`${process.env.MongodbServer}`, (err, then) => {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log("connected to db")
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("connected to mongoDB");
+  }
 });
+
+app.use(express.json());
+
+require("./Models/user");
+app.use(require("./Routes/auth"));
 
 app.listen("5000", () => console.log("server is running in 5000"));
