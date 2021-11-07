d9053d189d0faff299a05b6dd9dd0c1d0dfc5508
Abhinay babbi <30630095+Abhinaybabbi@users.noreply.github.com>
2021-11-07 06:47:17 +0530

    Backend completed

--- Server/middleware/requireLogin.js
+++ Server/middleware/requireLogin.js
@@ -1,25 +1,25 @@
+require('dotenv').config()
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
-// const User = mongoose.model("User")
+require("../Models/user");
+// const User = mongoose.model("User");
 const User = require("../Models/user");
-require('dotenv').config()
-module.exports = (req, res, next) => {
-    const { authorization } = req.headers
-    if (!authorization) {
-        return res.status(401).json({
-            error: "You must be logged in"
-        })
+
+module.exports = async (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ error: "unauthorized" });
+  }
+  const token = authorization.replace("Bearer ", "");
+  await jwt.verify(token, `${process.env.JWT_SECRET}`
+  , (err, payload) => {
+    if (err) {
+      return res.status(401).json({ error: "forbiden" });
     }
-    const token = authorization.replace("Bearer ", "")
-    jwt.verify(token, `${process.env.JWT_SECRET}`, (err, payload) => {
-        if (err) {
-            return res.status(401).json({ error: "you must logged in" })
-        }
-        const { _id } = payload
-        User.findById(_id).then(userdata => {
-            req.user = userdata
-            console.log(usedata)
-            next()
-        })
-    })
-}
+    const { _id } = payload;
+    User.find({ _id }).then((userdata) => {
+      req.user = userdata;
+      next();
+    });
+  });
+};
